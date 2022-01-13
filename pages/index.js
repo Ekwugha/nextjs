f746588c16b2eea77ebc1ce9409d4310fc0d6dc0
Ekwugha <elloekwugha@gmail.com>
2022-01-13 13:28:12 +0100

    created a layout for the footer and navbar component

--- pages/index.js
+++ pages/index.js
@@ -8,14 +8,12 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div>
-     <Navbar />
       <h1>Homepage</h1>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum autem ad quam cum laboriosam ex, magnam rem voluptas neque modi.</p>
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic quidem, accusantium quaerat aperiam quos optio voluptates nihil laboriosam saepe aliquam?</p>
       <Link href="/ninja">
-        <a> See Ninja Listing </a>
+        <a> See Ninja Listing </a>  
       </Link>
-      <Footer />
     </div>
   )
 }
