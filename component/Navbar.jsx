18f17f389de5e7f13000318d03320e5269a4aff2
Ekwugha <elloekwugha@gmail.com>
2022-01-13 13:16:40 +0100

    linking between the pages

--- component/Navbar.jsx
+++ component/Navbar.jsx
@@ -1,12 +1,14 @@
+import Link from 'next/link'
+
 const Navbar = () => {
     return ( 
         <nav>
-            <div className='logo'>
+            <div className="logo">
                 <h1>Ninja List</h1>
             </div>
-            <a>Home</a>
-            <a>Abut</a>
-            <a>Ninja Listing</a>
+            <Link href="/"><a>Home</a></Link>
+            <Link href="/about"><a>About</a></Link>
+            <Link href="/ninja"><a>Ninja Listing</a></Link>
         </nav>
      );
 }
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import Navbar from '../component/Navbar'
 import Footer from '../component/Footer'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -11,6 +12,9 @@ export default function Home() {
       <h1>Homepage</h1>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum autem ad quam cum laboriosam ex, magnam rem voluptas neque modi.</p>
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic quidem, accusantium quaerat aperiam quos optio voluptates nihil laboriosam saepe aliquam?</p>
+      <Link href="/ninja">
+        <a> See Ninja Listing </a>
+      </Link>
       <Footer />
     </div>
   )
