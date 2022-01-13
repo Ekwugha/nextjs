993f9af49370ff6b9f5cc8a53d8701b2b6ba3cf1
Ekwugha <elloekwugha@gmail.com>
2022-01-13 13:00:10 +0100

    removed error of additional spaces

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Navbar from '../component/Navbar'
-import Footer from '../component/Footer '
+import Footer from '../component/Footer'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
